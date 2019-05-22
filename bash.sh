037d2d9ae7f1e9cbe700d947ab743c64b5607861
Sean Wu <panda@ms5.url.com.tw>
2019-05-22 11:24:22 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 
+${parameter_name:-value} #假如parameter_name是空值，就回傳value，parameter_name還是空值
 
-
-${parameter_name:=value}  #假如parameter_name是空值，就指定value
+${parameter_name:=value}  #假如parameter_name是空值，就指定parameter_name為value
 if [ -z "$parameter_name" ]; then parameter_name=value; fi
 
 ! EXPRESSION:  test returns true of EXPRESSION is false
