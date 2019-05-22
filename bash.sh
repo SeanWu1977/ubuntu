d5317d574e6f9ea0eb793b9fe56dc60f6f467d26
Sean Wu <panda@ms5.url.com.tw>
2019-05-22 11:14:54 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,13 +1,24 @@
-```
-假如parameter_name是空值，就指定value
-${parameter_name:=value}
+
+
+
+${parameter_name:=value}  #假如parameter_name是空值，就指定value
 if [ -z "$parameter_name" ]; then parameter_name=value; fi
 
 ! EXPRESSION:  test returns true of EXPRESSION is false
 -z STRING: test returns true if the length of STRING is zero.
 
 
+set -o nounset  #遇到不存在的變量會報錯，預設為忽略. 
+set -u #同上
+
+set -x #一般只顯示執行結果，加此控制，會先输出執行的那行命令
+
+set +e  #執行時錯誤，不會停止
+set -e  #執行時錯誤，就停止
+
+set -o pipefail   #用pipe時，設定為有一個失敗即代表整行失敗。預設：最後一個執行失敗才算失敗
+
+# 一般用法
+set -euxo pipefail
 
 
-
-```
