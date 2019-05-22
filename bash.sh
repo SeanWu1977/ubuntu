9d1a5024ffd90f903475cc97e9a406ff58cfd485
Sean Wu <panda@ms5.url.com.tw>
2019-05-22 11:02:49 +0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,13 @@
+```
+假如parameter_name是空值，就指定value
+${parameter_name:=value}
+if [ -z "$parameter_name" ]; then parameter_name=value; fi
+
+! EXPRESSION:  test returns true of EXPRESSION is false
+-z STRING: test returns true if the length of STRING is zero.
+
+
+
+
+
+```
